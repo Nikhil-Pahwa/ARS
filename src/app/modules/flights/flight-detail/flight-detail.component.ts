67549ff2eb2dc783a3f6afa7bb7ca0a88e3d98b6
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-12-03 18:20:51 +0530

    Initial Commti

---
+++ src/app/modules/flights/flight-detail/flight-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-detail',
+  templateUrl: './flight-detail.component.html',
+  styleUrls: ['./flight-detail.component.css']
+})
+export class FlightDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/flights/flight-search/flight-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-search',
+  templateUrl: './flight-search.component.html',
+  styleUrls: ['./flight-search.component.css']
+})
+export class FlightSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
