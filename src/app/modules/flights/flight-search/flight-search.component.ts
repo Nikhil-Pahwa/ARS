718a81a726ed3c52a2bd6c1c9131cd837a6464f6
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-12-05 00:23:15 +0530

    [ADD] Listing added

--- src/app/modules/flights/flight-search/flight-search.component.ts
+++ src/app/modules/flights/flight-search/flight-search.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FlightService } from '../';
 import { Flight } from '../../../shared/resources/';
 
+declare let jQuery: any;
+
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
-  styleUrls: ['./flight-search.component.css'],
+  styleUrls: ['./flight-search.component.scss'],
   providers: [FlightService]
 })
 export class FlightSearchComponent implements OnInit {
@@ -15,6 +17,7 @@ export class FlightSearchComponent implements OnInit {
   constructor(private flightService: FlightService) { }
 
   ngOnInit() {
+    jQuery('#datepicker1').datepicker();
   }
 
   searchFlights() {
