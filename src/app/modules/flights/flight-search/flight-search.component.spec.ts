018b83386a7c371ea8c97dc636d114ace95c50d2
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-12-21 17:45:49 +0530

    [ADD] Added test cases

--- src/app/modules/flights/flight-search/flight-search.component.spec.ts
+++ src/app/modules/flights/flight-search/flight-search.component.spec.ts
@@ -7,13 +7,53 @@ import { HttpModule } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
+import { Filter } from '../../../shared/resources/';
 import { FlightService } from '../flight.service';
 import { FlightFilterComponent, HeaderService } from '../../../shared/components/';
 import { FlightSearchComponent } from './flight-search.component';
 
+let dummyFlights = [{
+  'origin': 'DEL',
+  'deptime': '13:20',
+  'duration': '1h 15m',
+  'airline': 'Air India',
+  'arrtime': '',
+  'stops': 0,
+  'logoUrl': 'assets/images/airlines/AI_sq.svg',
+  'flightno': 4,
+  'flightId': 'AI',
+  'destination': 'UDR',
+  'styleUrl': 'assets/css/external_domain_styles/air_india.css',
+  'fare': 4886,
+  'carrierId': 'AI',
+  'depDate': '12/21/2020',
+  'TravelTime': '',
+  'departureAirport': 'Indira Gandhi Intl.',
+  'arrivalAirport': 'Dabok'
+},
+{
+  'origin': 'DEL',
+  'deptime': '13:20',
+  'duration': '1h 15m',
+  'airline': 'Jet Airways',
+  'arrtime': '',
+  'stops': 0,
+  'logoUrl': 'assets/images/airlines/JA_sq.svg',
+  'flightno': 4,
+  'flightId': 'JA',
+  'destination': 'UDR',
+  'styleUrl': 'assets/css/external_domain_styles/air_india.css',
+  'fare': 5000,
+  'carrierId': 'AI',
+  'depDate': '12/21/2020',
+  'TravelTime': '',
+  'departureAirport': 'Indira Gandhi Intl.',
+  'arrivalAirport': 'Dabok'
+}];
+
 class MockFlightService {
   getFlightResults() {
-    return Observable.of([]);
+    return Observable.of(dummyFlights);
   }
 }
 
@@ -38,5 +78,42 @@ describe('FlightSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search flights for valid inputs', () => {
+    component.search = {
+      'from': 'Lisbon',
+      'to': 'Madrid',
+      'journeyDate': '12/31/2025'
+    };
+    (document.getElementById('datepicker1') as HTMLInputElement).value = '12/31/2025';
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      component.searchFlights();
+      expect(component.filteredList.length).toBe(2);
+    });
+  });
+
+  it('should not search flights for any invalid input', () => {
+    component.search = {
+      'from': 'Lisbon',
+      'to': 'Madrid',
+      'journeyDate': '12/31/2025'
+    };
+    (document.getElementById('datepicker1') as HTMLInputElement).value = '12/31/2025';
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      component.searchFlights();
+      expect(component.filteredList).toBeFalsy();
+    });
+  });
+
+  it('should filter flight results based on filter', () => {
+    let filters: Filter[] = [new Filter('Air India', 'AI', null, true)];
+    component.flightsList = dummyFlights;
+    component.filterSearch(filters);
+    expect(component.filteredList[0].carrierId).toBe('AI');
   });
 });
