edf1e8b3729c391acf2a8c99181bc2664fea157c
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-12-21 12:51:11 +0530

    [ADD] Added testbed for flight search component

--- src/app/modules/flights/flight-search/flight-search.component.spec.ts
+++ src/app/modules/flights/flight-search/flight-search.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LocalizationModule, LocaleService, LocaleConfig, LocaleStorage } from 'angular-l10n';
+import { HttpModule } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { FlightService } from '../flight.service';
+import { FlightFilterComponent, HeaderService } from '../../../shared/components/';
 import { FlightSearchComponent } from './flight-search.component';
+
+class MockFlightService {
+  getFlightResults() {
+    return Observable.of([]);
+  }
+}
 
 describe('FlightSearchComponent', () => {
   let component: FlightSearchComponent;
@@ -8,9 +23,11 @@ describe('FlightSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlightSearchComponent ]
+      declarations: [FlightSearchComponent, FlightFilterComponent],
+      imports: [FormsModule, LocalizationModule.forChild(), RouterModule, HttpModule],
+      providers: [HeaderService, LocaleService, LocaleConfig, LocaleStorage, { provide: FlightService, useClass: MockFlightService }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,1 +1,3 @@
-export * from './header/';
+export * from './header';
+export * from './flight-filter';
+export * from './payment-mode';
