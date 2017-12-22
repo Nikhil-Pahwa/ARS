dde648c1b5b66110d497c93687b3963d636dd62d
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-12-22 09:44:40 +0530

    [ADD] Added theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ars-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   // For future use
